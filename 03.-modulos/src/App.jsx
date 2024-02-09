de0facdb7b5b29e6c8730191ca99fb0b26439b67
Angel Armando Gonzalez Rodriguez <kuategonz@gmail.com>
2024-02-09 10:57:09 -0600

    se agrego los modulos

---
+++ 03.-modulos/src/App.jsx
@@ -0,0 +1,39 @@
+import ListItem from "./Listitem.jsx"
+const links =[
+  {
+  name: 'Google',
+  url: 'https://www.google.com/webhp?hl=es-419&sa=X&ved=0ahUKEwihoNLr2J6EAxUrl2oFHfWwC5IQPAgJ'
+  },
+  {
+    name: 'New Era',
+    url:'https://www.newera.mx/'
+  },
+  {name:'Facebook',
+  url:'https://www.facebook.com/?locale=es_LA'
+  }
+]
+function App() {
+
+  return (
+  <div>
+    <h1>Catalogo de link</h1>
+    <hr/>
+    <ul>
+{
+  links.map((link)=>{
+    <ListItem 
+    key={link.name}
+    name={link.name}
+    url={link.url}
+    />
+
+  }
+
+  )
+}
+    </ul>
+  </div>
+    )
+}
+
+export default App
