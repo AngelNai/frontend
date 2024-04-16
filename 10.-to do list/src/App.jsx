19d8b9ad861adf2849720c309355ee953fc8b121
Angel Armando Gonzalez Rodriguez <kuategonz@gmail.com>
2024-04-16 10:47:23 -0600

    se agrego tasklist

--- 10.-to do list/src/App.jsx
+++ 10.-to do list/src/App.jsx
@@ -1,5 +1,19 @@
-import { useState } from 'react'
-
+import List from "./List"
+const tasklist=[
+  {
+    id:1,
+    task:"estudiar",
+    limit:"15:00",
+  },{
+    id:2,
+    task:"practicar",
+    limit:"18:00",
+  },{
+    id:3,
+    task:"dormir",
+    limit:"23:00",
+  },
+]
 function App() {
 
   return (
@@ -7,33 +21,14 @@ function App() {
     <div>
     <h1>To Do List</h1>
     <hr/>
-    <div className="row">
-      <div className="col-6">
-<h4> Tasks</h4>
-      </div>
-      <div className="col-2">
-<h4>Limit </h4>
-      </div>
-      <div className="col">
-<h4> actions</h4>
-      </div>
-    </div>
-    <div className="row m-2">
-    <div className="col-6">
-      estudiar
-      </div>
-<div className='col-2'>
-13:00
-</div>
-<div className='col'>
-<button className='btn btn-sm btn-primary'>
-  edit
-</button>
-<button className='btn btn-sm btn-danger'>
-  delete
-  </button>
-  </div>
-  </div>
+    <List
+    
+    
+    tasklist={tasklist}
+    
+    
+    />
+    
   </div>
 
   )
