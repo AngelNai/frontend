7edf08e8e8cfe6d908ad82a025fd7794d3fe9206
Angel Armando Gonzalez Rodriguez <kuategonz@gmail.com>
2024-05-03 16:32:44 -0600

    WEATHER aPi

---
+++ 11.-weather_app/src/api/WeatherApi.js
@@ -0,0 +1,8 @@
+export const getWeather = async () => {
+    const key = "f5419501acf44ea2b00173047243004"
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Tuxtepec&aqi=no`
+    const response = await fetch(url)
+    const weather = await response.json()
+
+    console.log(weather)
+}
